e795331b47a3cb8b8a4e8459dd08993a3e8e98dd
Bagus Pangestu <baguspangestu@yandex.com>
2023-03-28 12:02:20 +0700

    Initial commit

---
+++ src/main/java/com/irvanudin/kuis/kuis/KuisApplication.java
@@ -0,0 +1,13 @@
+package com.irvanudin.kuis.kuis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KuisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KuisApplication.class, args);
+	}
+
+}
